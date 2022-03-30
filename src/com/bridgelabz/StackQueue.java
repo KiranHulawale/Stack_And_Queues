0693e612622cdca48c0cde1c663ccd3c08d4efea
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-30 23:30:02 +0530

    Update StackQueue.java

--- src/com/bridgelabz/StackQueue.java
+++ src/com/bridgelabz/StackQueue.java
@@ -3,13 +3,18 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 public class StackQueue {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList();
         list.push(70);
         list.push(30);
-        list.push(56);
-        list.display();
+        list.push(56);;
         list.peek();
+        list.display();
         list.pop();
+        System.out.println("Queue elements are:");
+        list.enqueue(56);
+        list.enqueue(30);
+        list.enqueue(70);
+        list.display();
     }
 }
