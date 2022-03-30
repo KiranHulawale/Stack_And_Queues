b1a311073ec82e3279d7db16821fd9928565d4b6
Kiran <kiranhulawale123@gmail.com>
2022-03-30 23:31:00 +0530

    Added UC 3 to create queue

---
+++ src/com/bridgelabz/INode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class INode {
+    int key;
+    INode next;
+
+    // constructor to create a new linked list node
+    public INode(int key)
+    {
+        this.key = key;
+        this.next = null;
+    }
+}
